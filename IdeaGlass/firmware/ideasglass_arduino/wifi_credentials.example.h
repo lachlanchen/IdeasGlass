2915e5cd803ac9ea4b1679cabbdd54578d70feb8
Lachlan Chen <lachlan@lazying.art>
2025-11-08 16:02:14 +0800

    Set wifi_credentials example to dd-wrt test network

--- IdeaGlass/firmware/ideasglass_arduino/wifi_credentials.example.h
+++ IdeaGlass/firmware/ideasglass_arduino/wifi_credentials.example.h
@@ -7,8 +7,7 @@ struct WifiCredential {
 
 // Copy this file to wifi_credentials.h and edit the values.
 static const WifiCredential WIFI_NETWORKS[] = {
-    {"MyNetwork", "super-secret-pass"},
-    {"BackupNetwork", "hunter2"},
+    {"dd-wrt", "512cmmmm"},
 };
 
 static constexpr size_t WIFI_NETWORK_COUNT = sizeof(WIFI_NETWORKS) / sizeof(WIFI_NETWORKS[0]);
